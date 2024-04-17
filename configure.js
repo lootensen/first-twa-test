8c6bd8a93db64cfb5b98bd43f8dca7c2f18c943e
lootensen <lootensen@proton.me>
2024-04-17 02:22:51 +0200

    change h3

--- configure.js
+++ configure.js
@@ -70,7 +70,7 @@ let githubUsername, githubRepo, botUsername;
     {
       menu_button: {
         type: "web_app",
-        text: "Launch Webapp",
+        text: "Launch OGREX",
         web_app: {
           url: url,
         },
